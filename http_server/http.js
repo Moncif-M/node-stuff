6ad159383d0c6d836bf40ef98be8f349d091a927
Viber <mouadmoncif1@gmail.com>
2026-09-20 22:10:25 +0100

    more scripts

--- http_server/http.js
+++ http_server/http.js
@@ -9,17 +9,38 @@ const port = process.env.PORT || 3000
 // })
 //
 
-const server = http.createServer((req, res) => {
-    let data = ""
-    req.on("data", (chunck) => {
-        data += chunck
-    })
-    req.on("end", () => {
-        console.log(JSON.parse(data).todo)
-        res.end()
-    })
+// const server = http.createServer((req, res) => {
+//     let data = ""
+//     req.on("data", (chunck) => {
+//         data += chunck
+//     })
+//     req.on("end", () => {
+//         console.log(JSON.parse(data).todo)
+//         res.end()
+//     })
+// })
+//
+
+const server = http.createServer(async (req, res) => {
+    const buffers = []
+    
+    for await (const chunck of req) {
+        buffers.push(chunck)
+    }
+    console.log(buffers)
+    console.log(Buffer.concat(buffers))
+
+    const data = Buffer.concat(buffers).toString()
+
+    console.log(JSON.parse(data).todo)
+    res.end()
 })
 
 server.listen(port, () => {
     console.log(`server running at port ${port}`)
 })
+
+
+
+
+
